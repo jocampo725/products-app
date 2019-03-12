0a6ca8ad4c4ee69430e8cb36989c6d2ff58e3c04
Jonathan Ocampo <jalexander1819@gmail.com>
2019-03-12 17:01:25 -0500

    uploading changes

--- products-app/src/components/categoriesPanel.js
+++ products-app/src/components/categoriesPanel.js
@@ -17,17 +17,17 @@ class CategoriesPanel extends Component {
         <div className="list__container">
           <List className="list__container-filters md-paper md-paper--1">
             <Subheader primary primaryText="Categories" />
-            <Link to={routes.PRODUCTS}>
+            <Link to={routes.PRODUCTS} className="list__container--link">
               <ListItem primaryText="All" leftAvatar={<Avatar icon={<FontIcon iconClassName="fa fa-list-ul" />} />} />
             </Link>
             <Divider />
-            <Link to={`${routes.PRODUCTS}/Tech`}>
+            <Link to={`${routes.PRODUCTS}/Tech`} className="list__container--link">
               <ListItem primaryText="Tech" leftAvatar={<Avatar icon={<FontIcon iconClassName="fa fa-laptop" />} />} />
             </Link>
-            <Link to={`${routes.PRODUCTS}/Services`}>
+            <Link to={`${routes.PRODUCTS}/Services`} className="list__container--link">
               <ListItem primaryText="Services" leftAvatar={<Avatar icon={<FontIcon iconClassName="fa fa-wrench" />} />} />
             </Link>
-            <Link to={`${routes.PRODUCTS}/Office`}>
+            <Link to={`${routes.PRODUCTS}/Office`} className="list__container--link">
               <ListItem primaryText="Office" leftAvatar={<Avatar icon={<FontIcon iconClassName="fa fa-folder" />} />} />
             </Link>
           </List>
--- products-app/src/components/productCard.js
+++ products-app/src/components/productCard.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Card, CardTitle, CardText, Grid, Cell } from 'react-md';
 
+const textcenter = {
+  textAlign: "center"
+}
 class ProductCard extends Component {
   render() {
     return (
       <Card className="md-block card__product">
-        <CardTitle title={this.props.name} subtitle={this.props.categories.join(" - ") + " - " + this.props.brand} />
+        <CardTitle className="card__product-title" title={this.props.name} subtitle={this.props.categories.join(" - ") + " - " + this.props.brand} />
         <CardText>
           <Grid className="grid-example">
-            <Cell size={4}>
-              <img src={this.props.photo} width="160px" alt={this.props.name} />
+            <Cell size={4} style={textcenter}>
+              <img src={this.props.photo} width="140px" alt={this.props.name} />
             </Cell>
             <Cell size={8}>
               <p>{this.props.description}</p>
